6b45d6331147459ca4e3c6900c7947c5b7dcff32
Sandip Mahato <65347607+SandipMahato89@users.noreply.github.com>
2023-09-16 18:19:04 +0000

    conversion of data_types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+                    /* conversion of data_types in JavaScript */
+
+// let score = 33       // Number 33
+// let score = "33"     // Number 33
+// let score = "33abc"      // Number NaN
+// let score = null     // Number 1
+// let score = undefined   // Number NaN
+// let score = true    // Number 1
+let score = "sandip"  // Number NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = "sandip"  // true
+// let isLoggedIn = ""     // false
+let isLoggedIn = 1      // 1 => true; 0 => false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let num = 25
+let numberToString = String(num)
+console.log(typeof num);
+console.log(typeof numberToString);
+console.log(numberToString);
