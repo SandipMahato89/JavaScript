d9816619cfcb9b00072fec74c5c99356e8b7daac
Sandip Mahato <65347607+SandipMahato89@users.noreply.github.com>
2023-09-16 15:58:52 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// allert( 3 + 3) //we are using nodejs, not browser
+
+let name = "sandip"
+let age = 23
+let isLoggedIn = true
+let state;
+
+// number => range 2^53
+// bigint
+// string => "", ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // output - undefined
+console.log(typeof null); // output - object
