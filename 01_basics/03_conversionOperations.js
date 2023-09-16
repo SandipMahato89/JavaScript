4fdc0a58b324bc5c76ff558e3dc8929d2a0e0674
Sandip Mahato <65347607+SandipMahato89@users.noreply.github.com>
2023-09-16 18:49:17 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,8 +25,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let num = 25
 let numberToString = String(num)
-console.log(typeof num);
-console.log(typeof numberToString);
-console.log(numberToString);
+// console.log(typeof num);
+// console.log(typeof numberToString);
+// console.log(numberToString);
 
 
+// ************** Operations **************
+
+let value = 10
+let negValue = -value
+// console.log(negValue);
+
+// console.log(5 + 5);
+// console.log(5 - 5);
+// console.log(5 * 5);
+// console.log(5 / 5);
+// console.log(5 % 5);
+// console.log(5**5);
+
+let str1 = "hello"
+let str2 = " sandip"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2" + 1);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let counter = 10
+++counter   // prefix -> first change the value then use the value
+counter++   // postfix -> first use the value then change the value
+console.log(counter);
+
+// conversion link for detailed explanation
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
