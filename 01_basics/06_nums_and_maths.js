585f2015a058d4cd6f4c646362c84181d6c33176
Sandip Mahato <65347607+SandipMahato89@users.noreply.github.com>
2023-09-17 18:31:17 +0000

    number and math in js

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+const score = 200
+// console.log(score);
+
+const balance = new Number(2500)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.881678
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+ // ************* Maths ******************
+
+
+ console.log(Math);
+ console.log(Math.abs(-2));
+ console.log(Math.round(4.5));
+ console.log(Math.ceil(4.2));
+ console.log(Math.floor(4.9));
+ console.log(Math.min(7,3,8,6,9,1,5));
+ console.log(Math.max(7,3,8,6,9,1,5));
+
+console.log(Math.random());
+ console.log((Math.random()*10) + 1);
+ console.log(Math.floor(Math.random()*10) + 1);
+
+ const min = 10
+ const max = 20
+
+ console.log(Math.floor(Math.random()*(max - min +1)) + min);
