9eede5bc89b701a951766cf7cf0d740e191e29b0
Sandip Mahato <65347607+SandipMahato89@users.noreply.github.com>
2023-09-22 04:00:49 +0000

    reduce filter map in js

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -2,9 +2,30 @@
 
 const myNums = [1, 2, 3, 4]
 
-const total = myNums.reduce( function (acc, curVal) {
-    console.log(`acc: ${acc} and curVal : ${curVal}`);
-    return acc +curVal 
-}, 5)
+// const total = myNums.reduce( function (acc, curVal) {
+//     console.log(`acc: ${acc} and curVal : ${curVal}`);
+//     return acc +curVal 
+// }, 5)
+
+const total = myNums.reduce( (acc, curr) => acc+curr, 5)
 
 console.log(total);
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 1500
+    },
+    {
+        itemName : "Mobile dev course",
+        price : 5000
+    },
+    {
+        itemName : "AI & ML",
+        price : 10000
+    }
+]
+
+const totalPrice = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(totalPrice);
